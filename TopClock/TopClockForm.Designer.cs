85db7402d3c81e4f3ff94d824f1880b2f31f7cab
Davide Bertoletti <daxliar@gmail.com>
2016-02-17 00:53:25 +0000

    First import

---
+++ TopClock/TopClockForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace TopClock
+{
+    partial class TopClockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TopClockForm));
+            this.g_timer = new System.Windows.Forms.Timer(this.components);
+            this.g_timeLabel = new System.Windows.Forms.Label();
+            this.g_btnPlusH = new System.Windows.Forms.Button();
+            this.g_btnMinusH = new System.Windows.Forms.Button();
+            this.g_mainPanel = new System.Windows.Forms.Panel();
+            this.g_cbxStart = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.g_cmbUpdateTime = new System.Windows.Forms.ComboBox();
+            this.g_btnReset = new System.Windows.Forms.Button();
+            this.g_btnMinusM = new System.Windows.Forms.Button();
+            this.g_btnPlusM = new System.Windows.Forms.Button();
+            this.g_mainPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // g_timeLabel
+            // 
+            this.g_timeLabel.AutoSize = true;
+            this.g_timeLabel.Font = new System.Drawing.Font("Verdana", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.g_timeLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.g_timeLabel.Location = new System.Drawing.Point(10, 38);
+            this.g_timeLabel.Name = "g_timeLabel";
+            this.g_timeLabel.Size = new System.Drawing.Size(302, 97);
+            this.g_timeLabel.TabIndex = 0;
+            this.g_timeLabel.Text = "00:00";
+            this.g_timeLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.g_timeLabel_MouseDown);
+            this.g_timeLabel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.g_timeLabel_MouseUp);
+            // 
+            // g_btnPlusH
+            // 
+            this.g_btnPlusH.Location = new System.Drawing.Point(36, 12);
+            this.g_btnPlusH.Name = "g_btnPlusH";
+            this.g_btnPlusH.Size = new System.Drawing.Size(95, 23);
+            this.g_btnPlusH.TabIndex = 1;
+            this.g_btnPlusH.Text = "+";
+            this.g_btnPlusH.UseVisualStyleBackColor = true;
+            this.g_btnPlusH.Click += new System.EventHandler(this.g_btnPlusH_Click);
+            // 
+            // g_btnMinusH
+            // 
+            this.g_btnMinusH.Location = new System.Drawing.Point(36, 138);
+            this.g_btnMinusH.Name = "g_btnMinusH";
+            this.g_btnMinusH.Size = new System.Drawing.Size(95, 23);
+            this.g_btnMinusH.TabIndex = 2;
+            this.g_btnMinusH.Text = "-";
+            this.g_btnMinusH.UseVisualStyleBackColor = true;
+            this.g_btnMinusH.Click += new System.EventHandler(this.g_btnMinusH_Click);
+            // 
+            // g_mainPanel
+            // 
+            this.g_mainPanel.Controls.Add(this.g_cbxStart);
+            this.g_mainPanel.Controls.Add(this.label1);
+            this.g_mainPanel.Controls.Add(this.g_cmbUpdateTime);
+            this.g_mainPanel.Controls.Add(this.g_btnReset);
+            this.g_mainPanel.Controls.Add(this.g_timeLabel);
+            this.g_mainPanel.Controls.Add(this.g_btnMinusM);
+            this.g_mainPanel.Controls.Add(this.g_btnPlusM);
+            this.g_mainPanel.Controls.Add(this.g_btnMinusH);
+            this.g_mainPanel.Controls.Add(this.g_btnPlusH);
+            this.g_mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.g_mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.g_mainPanel.Name = "g_mainPanel";
+            this.g_mainPanel.Size = new System.Drawing.Size(321, 303);
+            this.g_mainPanel.TabIndex = 3;
+            // 
+            // g_cbxStart
+            // 
+            this.g_cbxStart.Appearance = System.Windows.Forms.Appearance.Button;
+            this.g_cbxStart.Location = new System.Drawing.Point(36, 183);
+            this.g_cbxStart.Name = "g_cbxStart";
+            this.g_cbxStart.Size = new System.Drawing.Size(249, 30);
+            this.g_cbxStart.TabIndex = 9;
+            this.g_cbxStart.Text = "Click to &START";
+            this.g_cbxStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.g_cbxStart.UseVisualStyleBackColor = true;
+            this.g_cbxStart.CheckedChanged += new System.EventHandler(this.g_cbxStart_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Update every (minutes):";
+            // 
+            // g_cmbUpdateTime
+            // 
+            this.g_cmbUpdateTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.g_cmbUpdateTime.FormattingEnabled = true;
+            this.g_cmbUpdateTime.Items.AddRange(new object[] {
+            "1",
+            "5",
+            "10",
+            "15",
+            "30",
+            "60"});
+            this.g_cmbUpdateTime.Location = new System.Drawing.Point(203, 258);
+            this.g_cmbUpdateTime.Name = "g_cmbUpdateTime";
+            this.g_cmbUpdateTime.Size = new System.Drawing.Size(82, 24);
+            this.g_cmbUpdateTime.TabIndex = 7;
+            // 
+            // g_btnReset
+            // 
+            this.g_btnReset.Location = new System.Drawing.Point(36, 219);
+            this.g_btnReset.Name = "g_btnReset";
+            this.g_btnReset.Size = new System.Drawing.Size(249, 30);
+            this.g_btnReset.TabIndex = 6;
+            this.g_btnReset.Text = "&Reset";
+            this.g_btnReset.UseVisualStyleBackColor = true;
+            this.g_btnReset.Click += new System.EventHandler(this.g_btnReset_Click);
+            // 
+            // g_btnMinusM
+            // 
+            this.g_btnMinusM.Location = new System.Drawing.Point(190, 139);
+            this.g_btnMinusM.Name = "g_btnMinusM";
+            this.g_btnMinusM.Size = new System.Drawing.Size(95, 23);
+            this.g_btnMinusM.TabIndex = 3;
+            this.g_btnMinusM.Text = "-";
+            this.g_btnMinusM.UseVisualStyleBackColor = true;
+            this.g_btnMinusM.Click += new System.EventHandler(this.g_btnMinusM_Click);
+            // 
+            // g_btnPlusM
+            // 
+            this.g_btnPlusM.Location = new System.Drawing.Point(190, 13);
+            this.g_btnPlusM.Name = "g_btnPlusM";
+            this.g_btnPlusM.Size = new System.Drawing.Size(95, 23);
+            this.g_btnPlusM.TabIndex = 4;
+            this.g_btnPlusM.Text = "+";
+            this.g_btnPlusM.UseVisualStyleBackColor = true;
+            this.g_btnPlusM.Click += new System.EventHandler(this.g_btnPlusM_Click);
+            // 
+            // TopClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 303);
+            this.Controls.Add(this.g_mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TopClockForm";
+            this.Text = "TopClock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TopClockForm_FormClosing);
+            this.g_mainPanel.ResumeLayout(false);
+            this.g_mainPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer g_timer;
+        private System.Windows.Forms.Label g_timeLabel;
+        private System.Windows.Forms.Button g_btnPlusH;
+        private System.Windows.Forms.Button g_btnMinusH;
+        private System.Windows.Forms.Panel g_mainPanel;
+        private System.Windows.Forms.Button g_btnMinusM;
+        private System.Windows.Forms.Button g_btnPlusM;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox g_cmbUpdateTime;
+        private System.Windows.Forms.Button g_btnReset;
+        private System.Windows.Forms.CheckBox g_cbxStart;
+    }
+}
+
---
+++ TopClock/TopClockForm.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopClock
+{
+    public partial class TopClockForm : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Beep(uint dwFreq, uint dwDuration);
+        [DllImport("User32.dll")]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("User32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("User32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool EnumChildWindows(IntPtr hwndParent, EnumWindowsProc lpEnumFunc, IntPtr lParam);
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount );
+        [DllImport("User32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetDesktopWindow();
+
+        private Size originalFormSize = new Size();
+        private Point originalTimeLabelLocation = new Point();
+        private Color originalFormTransparencyKey = new Color();
+        private Color originalFormBackColor = new Color();
+        private FormBorderStyle originalFormBorderStyle = FormBorderStyle.None;
+        private Color originalStartCbxColor = new Color();
+
+        private Color originalTimerLabelForeColor = new Color();
+        private Color originalTimerLabelInvertedForeColor = new Color();
+        private Color originalTimerLabelBackColor = new Color();
+
+        private IntPtr shelltraywnd;
+        private IntPtr traynotifywnd;
+        private IntPtr trayclockwclass;
+
+        int WM_PAINT = 15;
+
+        public static List<IntPtr> GetChildWindows(IntPtr parent)
+        {
+            List<IntPtr> result = new List<IntPtr>();
+            GCHandle listHandle = GCHandle.Alloc(result);
+            try
+            {
+                EnumWindowsProc childProc = new EnumWindowsProc(EnumWindow);
+                EnumChildWindows(parent, childProc, GCHandle.ToIntPtr(listHandle));
+            }
+            finally
+            {
+                if (listHandle.IsAllocated)
+                    listHandle.Free();
+            }
+            return result;
+        }
+
+        private static bool EnumWindow(IntPtr handle, IntPtr pointer)
+        {
+            GCHandle gch = GCHandle.FromIntPtr(pointer);
+            List<IntPtr> list = gch.Target as List<IntPtr>;
+            if (list == null)
+            {
+                throw new InvalidCastException("GCHandle Target could not be cast as List<IntPtr>");
+            }
+            list.Add(handle);
+            // You can modify this to check to see if you want to cancel the operation, then return a null here
+            return true;
+        }
+
+        public static string GetWindowClassName(IntPtr hWnd)
+        {
+            StringBuilder buffer = new StringBuilder(128);
+
+            GetClassName(hWnd, buffer, buffer.Capacity);
+
+            return buffer.ToString();
+        }
+
+        public TopClockForm()
+        {
+            InitializeComponent();
+
+            g_cmbUpdateTime.SelectedIndex = 0;
+
+            SaveOriginalState();
+
+            originalStartCbxColor = g_cbxStart.BackColor;
+
+            originalTimerLabelForeColor = g_timeLabel.ForeColor;
+            originalTimerLabelBackColor = Color.Transparent;            
+            originalTimerLabelInvertedForeColor = Color.FromArgb(g_timeLabel.ForeColor.ToArgb() ^ 0xffffff);
+
+            UpdateTimeLabel(timerTotalMinutes);
+
+            //shelltraywnd = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "shell_traywnd", string.Empty);
+            //traynotifywnd = FindWindowEx(shelltraywnd, IntPtr.Zero, "traynotifywnd", string.Empty);
+            //List<IntPtr> list = GetChildWindows(traynotifywnd);
+
+            List<IntPtr> list = GetChildWindows(GetDesktopWindow());
+
+            //trayclockwclass = FindWindowEx(traynotifywnd, IntPtr.Zero, "TrayClockWClass", string.Empty);
+
+            List<String> classed = new List<String>();
+            foreach (IntPtr ptr in list)
+            {
+                String classname = GetWindowClassName(ptr);
+
+                if (classname == "TrayClockWClass")
+                {
+                    trayclockwclass = ptr;
+                }
+            }
+
+            ShowWindow(trayclockwclass, 0);
+        }
+
+        private void TopClockForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ShowWindow(trayclockwclass, 1);
+        }
+
+        private void g_cbxStart_CheckedChanged(object sender, EventArgs e)
+        {
+            if (g_cbxStart.Checked)
+            {
+                Start();
+                g_cbxStart.Text = "Click to &PAUSE";
+            }
+            else
+            {
+                Pause();
+                g_cbxStart.Text = "Click to &START";
+            }
+        }
+
+        private void Start()
+        {   
+            g_cbxStart.BackColor = Color.Green;
+            g_cmbUpdateTime.Enabled = false;
+            g_btnMinusM.Enabled = false;
+            g_btnPlusM.Enabled = false;
+            g_btnMinusH.Enabled = false;
+            g_btnPlusH.Enabled = false;
+
+            g_timer.Interval = 60000;
+            g_timer.Enabled = true;
+            g_timer.Tick += new System.EventHandler(OnTimerEvent);
+            g_timer.Tick -= new System.EventHandler(OnTimerEndEvent);
+        }
+
+        private void Pause()
+        {
+            g_cbxStart.BackColor = originalStartCbxColor;
+            g_cmbUpdateTime.Enabled = true;
+            g_btnMinusM.Enabled = true;
+            g_btnPlusM.Enabled = true;
+            g_btnMinusH.Enabled = true;
+            g_btnPlusH.Enabled = true;
+
+            g_timer.Enabled = false;
+            g_timer.Tick -= new System.EventHandler(OnTimerEvent);
+            g_timer.Tick -= new System.EventHandler(OnTimerEndEvent);
+        }
+
+        static bool labelInvertColor = false;
+        private void OnTimerEndEvent(object sender, EventArgs e)
+        {
+            if (labelInvertColor )
+            {
+                g_timeLabel.ForeColor = originalTimerLabelInvertedForeColor;
+                g_timeLabel.BackColor = originalTimerLabelForeColor;
+            }
+            else
+            {
+                g_timeLabel.ForeColor = originalTimerLabelForeColor;
+                g_timeLabel.BackColor = originalTimerLabelInvertedForeColor;
+            }
+
+            labelInvertColor = !labelInvertColor;
+        }
+
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            if ( currentTimeM > 0 )
+            {
+                currentTimeM--;
+
+                if (currentTimeM == 0)
+                {
+                    g_cbxStart.Text = "&Finished! Click Reset!";
+                    g_cbxStart.Enabled = false;
+
+                    g_timer.Interval = 500;
+                    g_timer.Enabled = true;
+                    g_timer.Tick -= new System.EventHandler(OnTimerEvent);
+                    g_timer.Tick += new System.EventHandler(OnTimerEndEvent);                    
+                }
+            }
+
+            String value = (String)g_cmbUpdateTime.SelectedItem;
+            uint minutesToCheck = UInt32.Parse(value);
+
+            if (currentTimeM == 0 || 0 == ((currentTimeM % 60) % minutesToCheck) )
+            {
+                UpdateTimeLabel(currentTimeM);
+            }
+        }
+
+        private void g_btnReset_Click(object sender, EventArgs e)
+        {
+            g_timer.Enabled = false;
+            g_cbxStart.Enabled = true;
+            g_cbxStart.Checked = false;
+
+            currentTimeM = timerTotalMinutes;
+            UpdateTimeLabel(timerTotalMinutes);
+            
+            g_timeLabel.ForeColor = originalTimerLabelForeColor;
+            g_timeLabel.BackColor = originalTimerLabelBackColor;
+        }
+
+        private uint timerTotalMinutes = 0;
+        private uint currentTimeM = 0;
+
+        private void g_btnPlusH_Click(object sender, EventArgs e)
+        {
+            timerTotalMinutes += 60;
+            UpdateTimeLabel(timerTotalMinutes);
+            currentTimeM = timerTotalMinutes;
+        }
+
+        private void g_btnMinusH_Click(object sender, EventArgs e)
+        {
+            if (timerTotalMinutes>=60)
+                timerTotalMinutes -= 60;
+            UpdateTimeLabel(timerTotalMinutes);
+            currentTimeM = timerTotalMinutes;
+        }
+
+        private void g_btnPlusM_Click(object sender, EventArgs e)
+        {
+            timerTotalMinutes++;
+            UpdateTimeLabel(timerTotalMinutes);
+            currentTimeM = timerTotalMinutes;
+        }
+
+        private void g_btnMinusM_Click(object sender, EventArgs e)
+        {
+            if (timerTotalMinutes > 0)
+                timerTotalMinutes--;
+
+            UpdateTimeLabel(timerTotalMinutes);
+            currentTimeM = timerTotalMinutes;
+        }
+
+        private void UpdateTimeLabel(uint inTotalMinutes)
+        {
+            uint totalHours = (uint)Math.Floor((double)inTotalMinutes / 60);
+            uint totalMinutes = inTotalMinutes % 60;
+
+            g_timeLabel.Text = String.Format("{0,2}:{1,2}", totalHours.ToString("D2"), totalMinutes.ToString("D2"));
+        }
+
+        private void SaveOriginalState()
+        {
+            originalFormSize = Size;
+            originalTimeLabelLocation = g_timeLabel.Location;
+            originalFormTransparencyKey = TransparencyKey;
+            originalFormBackColor = BackColor;
+            originalFormBorderStyle = FormBorderStyle;
+        }
+
+        private void RestoreOriginalState()
+        {
+            Size = originalFormSize;
+            g_timeLabel.Location = originalTimeLabelLocation;
+            TransparencyKey = originalFormTransparencyKey;
+            BackColor = originalFormBackColor;
+            FormBorderStyle = originalFormBorderStyle;
+        }
+
+        private void HideInterface()
+        {
+            // Display the form as top most form.
+            TopMost = true;
+            TopLevel = true;
+
+            // label in the origin
+            g_timeLabel.Location = new Point(0, 0);
+
+            // set the form transparent
+            TransparencyKey = Color.Turquoise;
+            BackColor = Color.Turquoise;
+
+            // remove borders
+            FormBorderStyle = FormBorderStyle.None;
+
+            // set form size like the time label size
+            Size = g_timeLabel.Size;
+        }
+
+        private void ShowInterface()
+        {
+            // Not anymore TopMost
+            TopMost = false;
+            TopLevel = false;
+
+            RestoreOriginalState();
+        }
+
+        private bool IsBorderless()
+        {
+            return FormBorderStyle == System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void ToggleInterface()
+        {
+            if (IsBorderless())
+            {
+                ShowInterface();
+            }
+            else
+            {
+                HideInterface();
+            }
+        }
+
+        private void g_timeLabel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && IsBorderless() )
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void g_timeLabel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                ToggleInterface();
+            }
+        }
+    }
+}
